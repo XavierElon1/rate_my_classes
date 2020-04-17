fbebf22e051cb106f85ef52a15906a0f74a9ac9e
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2020-04-16 23:10:17 -0400

    added eslint for better code quality

--- ratemyclasses-app/src/App.js
+++ ratemyclasses-app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import RMCAppBar from './components/RMCAppBar/RMCAppBar'
-import { ThemeProvider } from '@material-ui/core/styles';
+import RMCAppBar from './components/RMCAppBar/RMCAppBar';
+import {ThemeProvider} from '@material-ui/core/styles';
 import RMCTheme from './common/RMCTheme';
 import './App.css';
 
 function App() {
-  return (
-    // set at the top of the tree to pass app's theme down its children
-    <ThemeProvider theme={RMCTheme}>
-      <div className="App">
-        <RMCAppBar/>
-      </div>
-    </ThemeProvider>
-  );
+	return (
+	// set at the top of the tree to pass app's theme down its children
+		<ThemeProvider theme={RMCTheme}>
+			<div className="App">
+				<RMCAppBar/>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
