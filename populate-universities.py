6b9f26b8d4533e642e859e05201e992a6e1c6129
Mario <mario.bocaletti@ge.com>
2020-04-22 12:23:04 -0500

    add script to populate universities

---
+++ populate-universities.py
@@ -0,0 +1,22 @@
+import csv
+import json
+import requests
+import sys
+
+if len(sys.argv) < 3:
+    print('ERROR: Need to pass path to file as first argument and ratemyclasses API url as second argument')
+    exit()
+url = sys.argv[2]
+with open(sys.argv[1]) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            data = {"university": row[1], "website": row[2]}
+            result = requests.post(url, data = json.dumps(data))
+            print(result)
+            line_count += 1
+    print(f'Processed {line_count} lines.')
