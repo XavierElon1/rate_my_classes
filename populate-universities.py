afe50b324d2b4b9bbba710186a3b5a03760632c3
Mario <mario.bocaletti@ge.com>
2020-04-22 12:40:52 -0500

    fix property name

--- populate-universities.py
+++ populate-universities.py
@@ -11,12 +11,8 @@ with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            data = {"university": row[1], "website": row[2]}
-            result = requests.post(url, data = json.dumps(data))
-            print(result)
-            line_count += 1
+        data = {"institution": row[1], "website": row[2]}
+        result = requests.post(url, data = json.dumps(data))
+        print(result)
+        line_count += 1
     print(f'Processed {line_count} lines.')
