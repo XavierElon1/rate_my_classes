47f2ad1fb933c440ecdcd03d8b220c707078a971
Jackie Thind <jackie.thind@gmail.com>
2020-04-25 17:35:16 -0700

    restructured clientside and updated courseInfo page with new routing

--- ratemyclasses-app/Routes.js
+++ ratemyclasses-app/Routes.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Switch, withRouter, Route } from "react-router-dom";
+import * as styles from "./app.styles";
+
 import RMCAppBar from "./components/RMCAppBar/RMCAppBar";
 import NavigationTabs from "./components/NavigationTabs/NavigationTabs";
 import LandingPage from "./pages/LandingPage";
 import RateCoursePage from "./pages/RateCoursePage";
 import RateUniversityPage from "./pages/RateUniversityPage";
-import CourseInfo from "./pages/CourseInfo";
-// import * as styles from "./app.styles";
-
-// import "./App.css";
 
 function Routes() {
   return (
@@ -17,9 +15,8 @@ function Routes() {
       <NavigationTabs />
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route exact path="/rate-course" component={RateCoursePage} />
-        <Route exact path="/rate-University" component={RateUniversityPage} />
-        <Route path="/courseInfo/:id" component={CourseInfo} />
+        <Route path="/rate-course" component={RateCoursePage} />
+        <Route path="/rate-University" component={RateUniversityPage} />
       </Switch>
     </div>
   );
--- ratemyclasses-app/src/dummyData/schools.js
+++ ratemyclasses-app/src/dummyData/schools.js
@@ -1,64 +1,78 @@
 export const schools = [
-	{
-		name: 'Oregon State University',
-		averageRating: 4.5,
-		courses: [
-			{
-				title: 'test1',
-				body: '',
-				averageRating: 1,
-				averageDifficulty: 2,
-				averageHoursPerWeek: 61,
-			},
-			{
-				title: 'test2',
-				body: '',
-				averageRating: 3,
-				averageDifficulty: 4,
-				averageHoursPerWeek: 62,
-			},
-			{
-				title: 'test3',
-				body: '',
-				averageRating: 4,
-				averageDifficulty: 3,
-				averageHoursPerWeek: 30,
-			},
-			{
-				title: 'test4',
-				body: '',
-				averageRating: 5,
-				averageDifficulty: 2,
-				averageHoursPerWeek: 45,
-			},
-		],
-	},
+  {
+    name: "Oregon State University",
+    averageRating: 4.5,
+    courses: [
+      {
+        id: 1,
+        title: "test1",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 1,
+        averageDifficulty: 2,
+        averageHoursPerWeek: 61,
+      },
+      {
+        id: 2,
+        title: "test2",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 3,
+        averageDifficulty: 4,
+        averageHoursPerWeek: 62,
+      },
+      {
+        id: 3,
+        title: "test3",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 4,
+        averageDifficulty: 3,
+        averageHoursPerWeek: 30,
+      },
+      {
+        id: 4,
+        title: "test4",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 5,
+        averageDifficulty: 2,
+        averageHoursPerWeek: 45,
+      },
+    ],
+  },
 
-	{
-		name: 'San Jose State University',
-		averageRating: 4.5,
-		courses: [
-			{
-				title: 'test5',
-				body: '',
-				averageRating: 1,
-				averageDifficulty: 2,
-				averageHoursPerWeek: 61,
-			},
-			{
-				title: 'test6',
-				body: '',
-				averageRating: 3,
-				averageDifficulty: 4,
-				averageHoursPerWeek: 62,
-			},
-			{
-				title: 'test7',
-				body: '',
-				averageRating: 4,
-				averageDifficulty: 3,
-				averageHoursPerWeek: 30,
-			},
-		],
-	},
+  {
+    name: "San Jose State University",
+    averageRating: 4.5,
+    courses: [
+      {
+        id: 5,
+        title: "test5",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 1,
+        averageDifficulty: 2,
+        averageHoursPerWeek: 61,
+      },
+      {
+        id: 6,
+        title: "test6",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 3,
+        averageDifficulty: 4,
+        averageHoursPerWeek: 62,
+      },
+      {
+        id: 7,
+        title: "test7",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate. Et malesuada fames ac turpis egestas. Volutpat sed cras ornare arcu dui vivamus arcu felis. Interdum varius sit amet mattis vulputate enim nulla aliquet porttitor. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Vitae auctor eu augue ut lectus arcu. Mi ipsum faucibus vitae aliquet nec ullamcorper. Nisl pretium fusce id velit ut tortor. Ultrices sagittis orci a scelerisque purus semper. Fames ac turpis egestas sed tempus urna. At ultrices mi tempus imperdiet. Ac turpis egestas sed tempus urna et. Ultricies integer quis auctor elit sed vulputate mi sit. Feugiat nisl pretium fusce id velit ut. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Sed lectus vestibulum mattis ullamcorper velit. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Lacinia at quis risus sed vulputate.",
+        averageRating: 4,
+        averageDifficulty: 3,
+        averageHoursPerWeek: 30,
+      },
+    ],
+  },
 ];
