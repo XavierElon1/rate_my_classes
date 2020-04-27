1a85a65a0ed214bb6d4cc81c2e320a59a953627c
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2020-04-27 01:08:52 -0400

    added add course page

--- ratemyclasses-app/src/Routes.js
+++ ratemyclasses-app/src/Routes.js
@@ -1,28 +1,27 @@
-import React from "react";
-import { Switch, withRouter, Route } from "react-router-dom";
-import RMCAppBar from "./components/RMCAppBar/RMCAppBar";
-import NavigationTabs from "./components/NavigationTabs/NavigationTabs";
-import LandingPage from "./pages/LandingPage";
-import RateCoursePage from "./pages/RateCoursePage";
-import RateUniversityPage from "./pages/RateUniversityPage";
-import CourseInfo from "./pages/CourseInfo";
-// import * as styles from "./app.styles";
-
-// import "./App.css";
+import React from 'react';
+import {Switch, withRouter, Route} from 'react-router-dom';
+import RMCAppBar from './components/RMCAppBar/RMCAppBar';
+import NavigationTabs from './components/NavigationTabs/NavigationTabs';
+import LandingPage from './pages/LandingPage';
+import RateCoursePage from './pages/RateCoursePage';
+import RateUniversityPage from './pages/RateUniversityPage';
+import CourseInfo from './pages/CourseInfo';
+import AddCoursePage from './pages/AddCoursePage';
 
 function Routes() {
-  return (
-    <div className="App">
-      <RMCAppBar />
-      <NavigationTabs />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/rate-course" component={RateCoursePage} />
-        <Route exact path="/rate-University" component={RateUniversityPage} />
-        <Route path="/courseInfo/:id" component={CourseInfo} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<RMCAppBar />
+			<NavigationTabs />
+			<Switch>
+				<Route exact path='/' component={LandingPage} />
+				<Route exact path='/rate-course' component={RateCoursePage} />
+				<Route exact path='/add-course' component={AddCoursePage} />
+				<Route exact path='/rate-University' component={RateUniversityPage} />
+				<Route path='/courseInfo/:id' component={CourseInfo} />
+			</Switch>
+		</div>
+	);
 }
 
 export default withRouter(Routes);
