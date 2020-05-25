2f87ce65222db22d9d56caf3d062b19714bab741
Mario <mario.bocaletti@ge.com>
2020-05-25 16:09:27 -0500

    update script to use token and fix osu domain

--- populate-universities.py
+++ populate-universities.py
@@ -3,17 +3,18 @@ import json
 import requests
 import sys
 
-if len(sys.argv) < 3:
-    print('ERROR: Need to pass path to file as first argument and ratemyclasses API url as second argument')
+if len(sys.argv) < 4:
+    print('ERROR: Need to pass path to file as first argument, the ratemyclasses API url as second argument, and the token as the third')
     exit()
 url = sys.argv[2]
+token = sys.argv[3]
 with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         data = {"name":row[1], "website":row[2]}
         print(json.dumps(data))
-        result = requests.post(url, json=data)
+        result = requests.post(url, json=data, headers={'Authorization': 'Bearer ' + token})
         print(result)
         line_count += 1
     print(f'Processed {line_count} lines.')
