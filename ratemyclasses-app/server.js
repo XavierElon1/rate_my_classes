67fde45935ba4453da9625550949c84645f060fe
Xavier Elon Hollingsworth <Xavierh93@hotmail.com>
2020-05-27 12:57:42 -0400

    add dotenv

--- ratemyclasses-app/server.js
+++ ratemyclasses-app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+require("dotenv").config();
 
 const port = process.env.PORT || 8080;
 const app = express();
