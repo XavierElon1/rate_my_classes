e56845fd551cfb5a6b956cafc06cf960a710b1aa
Jackie Thind <jackie.thind@gmail.com>
2020-05-27 17:54:09 -0700

    fixed add review button and functionality

--- ratemyclasses-app/src/components/pages/CourseInfo.js
+++ ratemyclasses-app/src/components/pages/CourseInfo.js
@@ -11,9 +11,9 @@ import Spinner from '../../components/tools/Spinner';
 
 /*eslint-disable */
 const COURSES_URL =
-  process.env.REACT_APP_COURSES_URL || 'http://localhost:5000/courses';
+  process.env.REACT_APP_COURSES_URL || "http://localhost:5000/courses";
 const REVIEWS_URL =
-  process.env.REACT_APP_REVIEWS_URL || 'http://localhost:5000/reviews';
+  process.env.REACT_APP_REVIEWS_URL || "http://localhost:5000/reviews";
 /*eslint-disable */
 
 const useStyles = makeStyles((theme) => ({
@@ -25,173 +25,176 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
-  rateCourseBtn: {
-    position: 'fixed',
-    right: '10px',
-    top: '70px'
-  }
+  addReviewBtn: {
+    backgroundColor: theme.palette.secondary.main,
+  },
 }));
 
 function CourseInfo() {
-	const [selectedCourse, setSelectedCourse] = useState(null);
-	const [reviews, setReviews] = useState([]);
-  const {uniId, courseId} = useParams();
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [reviews, setReviews] = useState([]);
+  const { uniId, courseId } = useParams();
   const classes = useStyles();
   const history = useHistory();
-	const loadReviews = async () => {
-		try {
-			axios
-				.get(`${REVIEWS_URL}/${courseId}`, {
-					headers: new Headers({
-						Accept: 'application/json',
-					}),
-					crossdomain: true,
-				})
-				.then((res) => {
-					if (res.data) {
-						console.log(res.data);
-						setReviews(res.data.reviews);
-					}
-				});
-		} catch (e) {
-			setReviews(null);
-			console.error(e);
-		}
-	};
-	const loadSelectedCourse = async () => {
-		try {
-			axios
-				.get(`${COURSES_URL}/${uniId}/${courseId}`, {
-					headers: new Headers({
-						Accept: 'application/json',
-					}),
-					crossdomain: true,
-				})
-				.then((res) => {
-					if (res.data) {
-						console.log(res.data);
-						setSelectedCourse(res.data);
-					}
-				});
-		} catch (e) {
-			setSelectedCourse(null);
-			console.error(e);
-		}
-	};
+  const loadReviews = async () => {
+    try {
+      axios
+        .get(`${REVIEWS_URL}/${courseId}`, {
+          headers: new Headers({
+            Accept: "application/json",
+          }),
+          crossdomain: true,
+        })
+        .then((res) => {
+          if (res.data) {
+            console.log(res.data);
+            setReviews(res.data.reviews);
+          }
+        });
+    } catch (e) {
+      setReviews(null);
+      console.error(e);
+    }
+  };
+  const loadSelectedCourse = async () => {
+    try {
+      axios
+        .get(`${COURSES_URL}/${uniId}/${courseId}`, {
+          headers: new Headers({
+            Accept: "application/json",
+          }),
+          crossdomain: true,
+        })
+        .then((res) => {
+          if (res.data) {
+            console.log(res.data);
+            setSelectedCourse(res.data);
+          }
+        });
+    } catch (e) {
+      setSelectedCourse(null);
+      console.error(e);
+    }
+  };
 
-	useEffect(() => {
-		loadSelectedCourse();
-	}, []);
+  useEffect(() => {
+    loadSelectedCourse();
+  }, []);
 
-	useEffect(() => {
-		loadReviews();
-	}, [selectedCourse]);
+  useEffect(() => {
+    loadReviews();
+  }, [selectedCourse]);
 
-	if (!selectedCourse) {
-		return <div>Loading...</div>;
+  if (!selectedCourse) {
+    return <div>Loading...</div>;
   }
-  
+
   const addReviewTapped = () => {
-    history.push({pathname: '/rate-course', data: selectedCourse})
-  }
+    history.push({
+      pathname: "/rate-course/" + uniId + "/" + courseId,
+      data: selectedCourse,
+    });
+  };
 
-	const renderReviews = () => {
-		if (selectedCourse.reviews.length === 0) {
-			return null;
-		} else if (!selectedCourse.reviews) {
-			return <Spinner />;
-		} else {
-			return (
-				<React.Fragment>
-					{reviews.map((review) => {
-						var parsedDate = new Date(review.createdAt).toLocaleDateString();
-						console.log(parsedDate);
-						return (
-							<styles.Form marginBottom='1.5em' id={review._id}>
-								<styles.FlexRow
-									style={{alignItems: 'baseline', height: 'initial'}}
-								>
-									<styles.FlexColumn
-										style={{
-											width: '30%',
-											margin: '0px 20px',
-											alignItems: 'flex-start',
-											textAlign: 'left',
-											borderRight: '1px solid rgba(120, 120, 120, 0.5)',
-										}}
-									>
+  const renderReviews = () => {
+    if (selectedCourse.reviews.length === 0) {
+      return null;
+    } else if (!selectedCourse.reviews) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          {reviews.map((review) => {
+            var parsedDate = new Date(review.createdAt).toLocaleDateString();
+            console.log(parsedDate);
+            return (
+              <styles.Form marginBottom="1.5em" id={review._id}>
+                <styles.FlexRow
+                  style={{ alignItems: "baseline", height: "initial" }}
+                >
+                  <styles.FlexColumn
+                    style={{
+                      width: "30%",
+                      margin: "0px 20px",
+                      alignItems: "flex-start",
+                      textAlign: "left",
+                      borderRight: "1px solid rgba(120, 120, 120, 0.5)",
+                    }}
+                  >
                     Stats
-										<br />
+                    <br />
                     Difficulty: {review.difficulty} <br />
                     Grade: {review.grade} <br />
                     Hours per Week: {review.hoursPerWeek} <br />
                     Rating: {review.rating}
-										<br />
-									</styles.FlexColumn>
+                    <br />
+                  </styles.FlexColumn>
 
-									<styles.FlexColumn
-										style={{
-											width: '70%',
-											margin: '0px 20px',
-											alignItems: 'flex-start',
-											textAlign: 'left',
-										}}
-									>
-										{parsedDate} with Professor {review.professor} <br />
-										{review.body}
-									</styles.FlexColumn>
-								</styles.FlexRow>
-							</styles.Form>
-						);
-					})}
-				</React.Fragment>
-			);
-		}
-	};
-	
+                  <styles.FlexColumn
+                    style={{
+                      width: "70%",
+                      margin: "0px 20px",
+                      alignItems: "flex-start",
+                      textAlign: "left",
+                    }}
+                  >
+                    {parsedDate} with Professor {review.professor} <br />
+                    {review.body}
+                  </styles.FlexColumn>
+                </styles.FlexRow>
+              </styles.Form>
+            );
+          })}
+        </React.Fragment>
+      );
+    }
+  };
 
-	return (
-		<div className={styles.row}>
-			<div className={styles.col}>
-				<h1>{selectedCourse.title} - {selectedCourse.courseID}</h1>
-				<Grid container spacing={3}>
-					<Grid item xs>
-						<Paper className={styles.paper}>
-							<styles.H4>
-                Average Difficulty: {selectedCourse.averageDifficulty}
-							</styles.H4>
-						</Paper>
-					</Grid>
-					<Grid item xs={6}>
-						<Paper className={styles.paper}>
-							<styles.H3>
-                Average Rating: {selectedCourse.averageRating}
-							</styles.H3>
-						</Paper>
-					</Grid>
-					<Grid item xs>
-						<Paper className={styles.paper}>
-							<styles.H4>
-                Average Hours Per Week: {selectedCourse.averageHoursPerWeek}
-							</styles.H4>
-						</Paper>
-					</Grid>
-				</Grid>
-				<h2>Reviews</h2>
-				{renderReviews()}
-			</div>
-			<Button
+  return (
+    <div className={styles.row}>
+      <Button
         variant="contained"
         color="secondary"
-        size='small'
-				aria-label='add-rating'
-        className={classes.rateCourseBtn}
+        size="small"
+        aria-label="add-rating"
+        className={classes.addReviewBtn}
         onClick={addReviewTapped}
-        startIcon={<Create />}>
-        Rate Course
+        startIcon={<Create />}
+      >
+        Add Review
       </Button>
-		</div>
-	);
+      <div className={styles.col}>
+        <h1>
+          {selectedCourse.title} - {selectedCourse.courseID}
+        </h1>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Paper className={styles.paper}>
+              <styles.H4>
+                Average Difficulty: {selectedCourse.averageDifficulty}
+              </styles.H4>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={styles.paper}>
+              <styles.H3>
+                Average Rating: {selectedCourse.averageRating}
+              </styles.H3>
+            </Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={styles.paper}>
+              <styles.H4>
+                Average Hours Per Week: {selectedCourse.averageHoursPerWeek}
+              </styles.H4>
+            </Paper>
+          </Grid>
+        </Grid>
+        <h2>Reviews</h2>
+        {renderReviews()}
+      </div>
+    </div>
+  );
 }
 
 export default CourseInfo;
