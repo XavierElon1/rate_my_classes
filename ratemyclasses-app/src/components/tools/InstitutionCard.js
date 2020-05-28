89407c248cc32734631fa4420f64ff15a7c36ecf
Xavier Hollingsworth <xavierhollingsworth@Xaviers-MacBook-Pro-2.local>
2020-05-28 19:41:14 -0400

    fixed institution star issue

--- ratemyclasses-app/src/components/tools/InstitutionCard.js
+++ ratemyclasses-app/src/components/tools/InstitutionCard.js
@@ -21,7 +21,7 @@ function InstitutionCard({ institution: institution, random: random }) {
   const location = useLocation();
   const history = useHistory();
   const getRatingStars = () => {
-    var emptyStars = 5 - institution.averageRating;
+    var emptyStars = Math.floor(5 - institution.averageRating);
     var stars = [];
     for (var i = 0; i < institution.averageRating; i++) {
       stars.push(<Icon className={"MuiIcon--starred"}>star_rounded</Icon>);
