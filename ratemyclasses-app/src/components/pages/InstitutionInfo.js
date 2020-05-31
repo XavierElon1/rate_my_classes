4471e1c2bbc0844c0f731c499c1a4dd11ce5ce01
Jackie Thind <jackie.thind@gmail.com>
2020-05-30 18:44:28 -0700

    removing logs

--- ratemyclasses-app/src/components/pages/InstitutionInfo.js
+++ ratemyclasses-app/src/components/pages/InstitutionInfo.js
@@ -287,23 +287,10 @@ function InstitutionInfo() {
                     value={value}
                     text={`${selectedInstitution.averageRating}/5`}
                     styles={buildStyles({
-                      // Rotation of path and trail, in number of turns (0-1)
-
-                      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
                       strokeLinecap: "round",
-
-                      // Text size
                       textSize: "16px",
-
-                      // How long animation takes to go from one percentage to another, in seconds
                       pathTransition: "none",
-
-                      // Can specify path transition in more detail, or remove it entirely
-                      // pathTransition: 'none',
-
-                      // Colors
                       pathColor: `rgba(13, 127, 161, ${percentage / 100})`,
-
                       textColor: "#212121",
                       trailColor: "#d6d6d6",
                       backgroundColor: "#3e98c7",
